2516e49e9fc25cbebb153d1ce4c92015cb2c0b57
Lambels <patrick.arvatu@yahoo.com>
2023-02-08 21:19:04 +0200

    modularized

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, Read};
 use std::env;
+
+use minigrep::Worker;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
+    let mut buf = String::new();
 
-    let (f_name, search) = match parse_args(&args[1..]) {
+    let workers = match parse_args(&args[1..], &mut buf) {
         Ok(vals) => vals,
         Err(msg) => panic!("{}", msg),
     };
 
-    println!("Searching for {:?}", search);
-    println!("In: {}", f_name);
-}
-
-fn parse_args(args: &[String]) -> Result<(&String, &[String]), String> {
-    if args.len() < 2 {
-        return Err(format!(
-            "Expected at least two arguments but go {}",
-            args.len()
-        ));
+    let mut n: usize = 0;
+    for worker in workers {
+        n += worker.run(false, &mut io::stdout()).unwrap();
     }
 
-    Ok((&args[0], &args[1..]))
+    println!("Found {} matches!", n);
+}
+
+fn parse_args<'a>(args: &'a [String], buf: &'a mut String) -> Result<Vec<Worker<'a>>, Box<dyn Error>> {
+    if args.len() < 2 {
+        return Err("expected at least 2 arguments".into());
+    }
+
+    File::open(&args[0])?.read_to_string(buf)?;
+    let mut workers = Vec::with_capacity(args[1..].len());
+
+    for v in &args[1..] {
+        workers.push(Worker::new(buf, v.clone()));
+    }
+
+    Ok(workers)
 }
